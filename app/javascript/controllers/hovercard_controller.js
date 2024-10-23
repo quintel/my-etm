06137e8853250e5753baf8ad1e6b3a4515ef435f
noracato <ncschinkel@gmail.com>
2024-10-23 15:12:12 +0200

    Make hovercard more smooth

--- app/javascript/controllers/hovercard_controller.js
+++ app/javascript/controllers/hovercard_controller.js
@@ -6,9 +6,10 @@ export default class extends Controller {
 
   show() {
     if (this.hasCardTarget) {
-      this.cardTarget.classList.remove("opacity-0");
       this.cardTarget.classList.add("delay-500");
+      this.cardTarget.classList.remove("hidden");
       this.cardTarget.classList.add("opacity-100");
+      this.cardTarget.classList.remove("opacity-0");
     } else {
       fetch(this.urlValue)
         .then((r) => r.text())
@@ -27,6 +28,7 @@ export default class extends Controller {
       this.cardTarget.classList.add("opacity-0");
       this.cardTarget.classList.remove("delay-500");
       this.cardTarget.classList.remove("opacity-100");
+      this.cardTarget.classList.add("hidden");
     }
   }
 
