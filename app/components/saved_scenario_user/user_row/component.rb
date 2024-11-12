1b9defb144946d49731846dfefa76739100533b1
noracato <ncschinkel@gmail.com>
2024-11-12 16:13:41 +0100

    Copied manage access from ETModel and altered views to fit the style of my-etm

--- app/components/saved_scenario_user/user_row/component.rb
+++ app/components/saved_scenario_user/user_row/component.rb
@@ -35,3 +35,40 @@ module SavedScenarioUser::UserRow
     end
   end
 end
+module SavedScenarioUser::UserRow
+  class Component < ApplicationComponent
+    include Turbo::FramesHelper
+
+    option :user
+    option :destroy_path
+    option :update_path
+    option :destroyable, default: proc { true }
+    option :confirmed, default: proc { true }
+
+    # thing for rendering action
+
+    def css_classes
+      @confirmed ? "" : "!text-midnight-450"
+    end
+
+    def disabled
+      @destroyable ? {} : { disabled: true }
+    end
+
+    def disabled_classes
+      @destroyable ? "text-sm hover:cursor-pointer" : "text-sm bg-none bg-midnight-300 border-midnight-300"
+    end
+
+    def destroy_classes
+      @destroyable ? "" : "!hover:text-midnight-400 !hover:cursor-initial pointer-events-none"
+    end
+
+    def destroy_text
+      if @destroyable
+        t('saved_scenario_users.confirm_destroy.button')
+      else
+        t('saved_scenario_users.confirm_destroy.not_possible')
+      end
+    end
+  end
+end
