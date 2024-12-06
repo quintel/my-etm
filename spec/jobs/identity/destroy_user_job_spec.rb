ff2451e89d35b4f5eb7eb3a67c810448be9f6c56
noracato <ncschinkel@gmail.com>
2024-12-06 13:21:34 +0100

    Mock etengine in user job specs

--- spec/jobs/identity/destroy_user_job_spec.rb
+++ spec/jobs/identity/destroy_user_job_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Identity::DestroyUserJob, type: :job do
   context 'when Settings.etmodel_uri is set' do
     let(:user) { create(:user) }
     let(:connection) { instance_double(Faraday::Connection) }
+    let(:connection_engine) { instance_double(Faraday::Connection) }
 
     before do
       Settings.etmodel_uri = 'http://example.org'
@@ -13,7 +14,13 @@ RSpec.describe Identity::DestroyUserJob, type: :job do
         .with(user)
         .and_return(connection)
 
+      allow(MyEtm::Auth)
+        .to receive(:engine_client)
+        .with(user)
+        .and_return(connection_engine)
+
       allow(connection).to receive(:delete)
+      allow(connection_engine).to receive(:delete)
     end
 
     after do
@@ -23,6 +30,20 @@ RSpec.describe Identity::DestroyUserJob, type: :job do
     it 'sends a PUT request to the ETModel API' do
       described_class.perform_now(user.id)
       expect(connection).to have_received(:delete).with('/api/v1/user')
+    end
+
+    it 'sends a PUT request to the EEngine API' do
+      described_class.perform_now(user.id)
+      expect(connection_engine).to have_received(:delete).with('/api/v3/user')
+    end
+
+    it 'destroys the user' do
+      expect { described_class.perform_now(user.id) }
+        .to change { User.where(id: user.id).count }.by(-1)
+    end
+
+    it 'returns true' do
+      expect(described_class.perform_now(user.id)).to be(true)
     end
 
     it 'destroys the user' do
--- spec/jobs/identity/sync_user_job_spec.rb
+++ spec/jobs/identity/sync_user_job_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Identity::SyncUserJob, type: :job do
   context 'when Settings.etmodel_uri is set' do
     let(:user) { create(:user) }
     let(:connection) { instance_double(Faraday::Connection) }
+    let(:connection_engine) { instance_double(Faraday::Connection) }
 
     before do
       Settings.etmodel_uri = 'http://example.org'
@@ -13,7 +14,14 @@ RSpec.describe Identity::SyncUserJob, type: :job do
         .with(user)
         .and_return(connection)
 
+      allow(MyEtm::Auth)
+        .to receive(:engine_client)
+        .with(user)
+        .and_return(connection_engine)
+
+
       allow(connection).to receive(:put)
+      allow(connection_engine).to receive(:put)
     end
 
     after do
@@ -23,6 +31,11 @@ RSpec.describe Identity::SyncUserJob, type: :job do
     it 'sends a PUT request to the ETModel API' do
       described_class.perform_now(user.id)
       expect(connection).to have_received(:put).with('/api/v1/user', anything)
+    end
+
+    it 'sends a PUT request to the ETEngine API' do
+      described_class.perform_now(user.id)
+      expect(connection_engine).to have_received(:put).with('/api/v3/user', anything)
     end
 
     it 'returns true' do
