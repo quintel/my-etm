0cf47dc54ad9a174fe8b48f434aebd0784b16116
noracato <ncschinkel@gmail.com>
2024-12-16 12:45:13 +0100

    Add todos for later

--- app/models/featured_scenario_user.rb
+++ app/models/featured_scenario_user.rb
@@ -1,4 +1,6 @@
 class FeaturedScenarioUser < ApplicationRecord
+  # TODO user should be optional. Either User or Name.
+  # TODO form to create Featured scenario users from admin console!
   belongs_to :user
   has_many :featured_scenarios, foreign_key: :owner_id, dependent: :destroy
 
