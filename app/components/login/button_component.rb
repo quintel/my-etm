106732d73360211814a49ef145b5cddb25b357ae
noracato <ncschinkel@gmail.com>
2024-12-17 11:35:08 +0100

    Update button styles

--- app/components/login/button_component.rb
+++ app/components/login/button_component.rb
@@ -3,7 +3,7 @@
 module Login
   class ButtonComponent < ActionButtonComponent
     def initialize(form:)
-      super(form:, type: :submit, color: :primary, size: :lg, class: "w-full !py-3 mt-5 bg-midnight-600")
+      super(form:, type: :submit, color: :primary, size: :lg, class: "w-full !py-3 mt-5 bg-midnight-600 text-midnight-800")
     end
   end
 end
