b6228f6569e38278bc2dc26c22ecbba8e6b7a137
noracato <ncschinkel@gmail.com>
2024-12-17 12:26:10 +0100

    Setup versions APi to communicate with ETModel

--- app/models/version.rb
+++ app/models/version.rb
@@ -2,13 +2,25 @@
 
 # A valid version of the ETM
 class Version
-  LIST = %w[
-    latest
-  ].freeze
+  URL = "energytransitionmodel.com".freeze
+
+  LIST = {
+    "latest" => "https://#{Version::URL}",
+    "stable.01" => "https://stable.#{Version::URL}",
+    "stable.02" => "https://stable2.#{Version::URL}"
+  }.freeze
 
   # All available versions. Uses ActiveRecord syntax 'all' to
   # make future porting to db easier
   def self.all
     LIST
+  end
+
+  def self.tags
+    LIST.keys
+  end
+
+  def self.as_json(*)
+    LIST.map { |tag, url| { tag: tag, url: url } }
   end
 end
