b87323b5ea6231329d12a3b815d5f1e3bc3360e3
noracato <ncschinkel@gmail.com>
2025-01-10 15:51:36 +0100

    Update Open scenario query params

--- app/components/saved_scenarios/info/component.rb
+++ app/components/saved_scenarios/info/component.rb
@@ -4,5 +4,13 @@ module SavedScenarios::Info
     option :saved_scenario
     option :time
     option :button_title
+
+    def link_to_scenario
+      params = {
+        scenario_id: @saved_scenario.scenario_id,
+        title: @saved_scenario.title
+      }
+      "#{@path}?#{params.to_query}"
+    end
   end
 end
