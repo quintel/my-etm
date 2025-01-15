c813b2dab09a0d3971c653a989548f98753580c3
louispt1 <louisparkestalbot1@gmail.com>
2025-01-15 14:44:04 +0100

    Updating CORS to allow requests to /oauth endpoints

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
+    resource '/oauth/*',
+      headers: :any,
+      methods: [:get, :post, :options],
+      credentials: true
+
+    # Retain API-specific configuration
     resource '/api/*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
