b9d1574c3d967f7c64ab6474192357b535e07532
louispt1 <louisparkestalbot1@gmail.com>
2025-01-15 15:28:05 +0100

    Simplified session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
-MyEtm::Application.config.session_store :active_record_store,
-                                        key: "_idp_session",
-                                        domain: '.energytransitionmodel.com',
-                                        secure: Rails.env.production?,
-                                        same_site: :none
+# MyEtm::Application.config.session_store :active_record_store,
+#                                         key: "_idp_session",
+#                                         domain: '.energytransitionmodel.com',
+#                                         secure: Rails.env.production?,
+#                                         same_site: :none
+
+Rails.application.config.session_store(:cookie_store, key: '_myetm')
