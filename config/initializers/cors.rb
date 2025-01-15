e0cdf84f6e735fa836fd3efd63a9d7a742db7c53
louispt1 <louisparkestalbot1@gmail.com>
2025-01-15 16:15:26 +0100

    reverting changes to CORS and settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins do |origin, _env|
-      # Allow requests from *.energytransitionmodel.com
-      origin =~ /\.energytransitionmodel\.com$/ || origin == 'energytransitionmodel.com'
-    end
-    resource '/oauth/*',
-      headers: :any,
-      methods: [:get, :post, :options],
-      credentials: true
-
-    # Retain API-specific configuration
+    origins '*'
     resource '/api/*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
