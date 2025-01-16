dd562536443da8bde845c451240f7871b2eedb16
louispt1 <louisparkestalbot1@gmail.com>
2025-01-16 11:16:44 +0100

    Fixing staging config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__)) + '/production'
 
-Rails::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in
   # config/application.rb
 end
