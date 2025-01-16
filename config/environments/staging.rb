ec63df7254ccd5e8e2a967557b4f0bb824d0f143
louispt1 <louisparkestalbot1@gmail.com>
2025-01-16 11:11:52 +0100

    Reformatting staging environment config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__)) + '/production'
 
-Etm::Application.configure do
+Rails::Application.configure do
   # Settings specified here will take precedence over those in
   # config/application.rb
 end
