7d26476dd2e5cc5873e7e9aaa662caa82302c481
louispt1 <louisparkestalbot1@gmail.com>
2025-01-21 13:34:53 +0100

    Sync and destroy user jobs work across all available versions.

--- spec/jobs/identity/sync_user_job_spec.rb
+++ spec/jobs/identity/sync_user_job_spec.rb
@@ -1,41 +1,50 @@
 # frozen_string_literal: true
 
 RSpec.describe Identity::SyncUserJob, type: :job do
-  context 'when Settings.etmodel_uri is set' do
+  context 'when Settings.etmodel_uri and Settings.etengine_uri are set' do
     let(:user) { create(:user) }
-    let(:connection) { instance_double(Faraday::Connection) }
-    let(:connection_engine) { instance_double(Faraday::Connection) }
+    let(:versions) { create_list(:version, 3) } # Mocking three versions for the test
+    let(:connections) { versions.map { instance_double(Faraday::Connection) } }
+    let(:engine_connections) { versions.map { instance_double(Faraday::Connection) } }
 
     before do
       Settings.etmodel_uri = 'http://example.org'
+      Settings.etengine_uri = 'http://example.com'
 
-      allow(MyEtm::Auth)
-        .to receive(:model_client)
-        .with(user)
-        .and_return(connection)
+      allow(Version).to receive(:all).and_return(versions)
 
-      allow(MyEtm::Auth)
-        .to receive(:engine_client)
-        .with(user)
-        .and_return(connection_engine)
+      versions.each_with_index do |version, index|
+        allow(MyEtm::Auth)
+          .to receive(:model_client)
+          .with(user, version)
+          .and_return(connections[index])
 
+        allow(MyEtm::Auth)
+          .to receive(:engine_client)
+          .with(user, version)
+          .and_return(engine_connections[index])
 
-      allow(connection).to receive(:put)
-      allow(connection_engine).to receive(:put)
+        allow(connections[index]).to receive(:put)
+        allow(engine_connections[index]).to receive(:put)
+      end
     end
 
     after do
       Settings.reload!
     end
 
-    it 'sends a PUT request to the ETModel API' do
+    it 'sends a PUT request to the ETModel API for each version' do
       described_class.perform_now(user.id)
-      expect(connection).to have_received(:put).with('/api/v1/user', anything)
+      versions.each_with_index do |_, index|
+        expect(connections[index]).to have_received(:put).with('/api/v1/user', anything)
+      end
     end
 
-    it 'sends a PUT request to the ETEngine API' do
+    it 'sends a PUT request to the ETEngine API for each version' do
       described_class.perform_now(user.id)
-      expect(connection_engine).to have_received(:put).with('/api/v3/user', anything)
+      versions.each_with_index do |_, index|
+        expect(engine_connections[index]).to have_received(:put).with('/api/v3/user', anything)
+      end
     end
 
     it 'returns true' do
