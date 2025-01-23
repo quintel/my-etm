032856bd4b4e6c2a696b18621ec21f550340729e
noracato <ncschinkel@gmail.com>
2025-01-23 14:18:04 +0100

    Add form for historical version

--- app/components/history/row/component.rb
+++ app/components/history/row/component.rb
@@ -5,5 +5,13 @@ module History::Row
     option :historical_version
     option :tag
     option :update_path
+
+    def description
+      if @historical_version.description.presence
+        @historical_version.description
+      else
+        t("history.empty_message")
+      end
+    end
   end
 end
