623a8bd79abd37d15aa92c3fc1af6214dc5ad44f
louispt1 <louisparkestalbot1@gmail.com>
2025-01-30 14:07:02 +0100

    Setup exchange token endpoint to turn a PAT into a JWT

--- app/controllers/identity/access_tokens_controller.rb
+++ app/controllers/identity/access_tokens_controller.rb
@@ -15,6 +15,28 @@ module Identity
       end
     end
 
+    def exchange
+      authenticate_with_http_token do |token, _options|
+        @provided_pat = token
+      end || (return render_pat_error('Missing PAT'))
+
+      @client = Doorkeeper::Application.find_by(uid: params[:client_id])
+      return render_pat_error('Invalid client') unless @client
+
+      scopes = @client.scopes
+
+      oauth_token = Doorkeeper::AccessToken.find_by(token: @provided_pat)
+      return render_pat_error('Invalid PAT') unless oauth_token
+
+      pat = PersonalAccessToken.not_expired.find_by(oauth_access_token_id: oauth_token.id)
+      return render_pat_error('Invalid or expired PAT') unless pat
+
+      user = pat.user
+
+      jwt = MyEtm::Auth.user_jwt(user, scopes: scopes, client_id: @client.uid)
+      render json: { access_token: jwt, token_type: "Bearer", expires_in: 3600 }
+    end
+
     private
 
     def valid_client?
@@ -25,6 +47,14 @@ module Identity
     def generate_access_token
       scopes = @client.scopes
       MyEtm::Auth.user_jwt(@user, scopes: scopes, client_id: @client.uid)
+    end
+
+
+    def render_pat_error(message)
+      render json: {
+        error: 'invalid_request',
+        error_description: message
+      }, status: :unauthorized
     end
   end
 end
