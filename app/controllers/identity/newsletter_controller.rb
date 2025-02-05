58f2e6995f49b74640d24fe329d0530cfa7b0f46
louispt1 <louisparkestalbot1@gmail.com>
2025-02-05 15:40:31 +0100

    Change update to replace in update.turbo_stream for newsletters

--- app/controllers/identity/newsletter_controller.rb
+++ app/controllers/identity/newsletter_controller.rb
@@ -17,7 +17,7 @@ module Identity
 
       result = service.new.call(user: current_user, audience: @audience)
 
-      if result.success?
+      if result.successful?
         respond_to do |format|
           format.turbo_stream { render turbo_stream: turbo_stream.replace(@audience, partial: "identity/newsletter_status_row", locals: { audience: @audience }) }
           format.html { redirect_to identity_profile_path, notice: "Subscription updated." }
