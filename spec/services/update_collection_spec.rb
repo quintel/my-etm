0af4354458ef87f45477337476824ca81bc5fee4
louispt1 <louisparkestalbot1@gmail.com>
2025-02-07 12:02:54 +0100

    Update collection spec

--- spec/services/update_collection_spec.rb
+++ spec/services/update_collection_spec.rb
@@ -8,6 +8,8 @@ describe UpdateCollection, type: :service do
   let!(:saved_scenario) do
     FactoryBot.create(:saved_scenario, user: user, id: 900)
   end
+  let!(:saved_scenario17) { create(:saved_scenario,  user: user, id: 17) }
+  let!(:saved_scenario18) { create(:saved_scenario,  user: user, id: 18) }
 
   let(:settings) do
     {
@@ -15,7 +17,7 @@ describe UpdateCollection, type: :service do
       area_code: 'nl',
       end_year: 2050,
       scenario_ids: [101, 102],
-      saved_scenario_ids: [201, 202]
+      saved_scenario_ids: [17, 18]
     }
   end
 
@@ -38,20 +40,20 @@ describe UpdateCollection, type: :service do
 
     it 'updates the collection attributes' do
       expect(result.value.title).to eq('My Updated Title')
-      expect(result.value.area_code).to eq('us')
+      expect(result.value.area_code).to eq('nl')
       expect(result.value.end_year).to eq(2050)
     end
 
     it 'updates the scenario_ids on the collection' do
-      expect { result }.to change { collection.scenarios.count }.from(1).to(2)
-      updated_ids = result.value.collection_scenarios.pluck(:scenario_id)
+      expect { result }.to change { collection.scenarios.count }.from(3).to(2)
+      updated_ids = result.value.scenarios.pluck(:scenario_id)
       expect(updated_ids).to match_array([101, 102])
     end
 
     it 'updates the saved_scenario_ids on the collection' do
       expect { result }.to change { collection.collection_saved_scenarios.count }.from(1).to(2)
       updated_ids = result.value.collection_saved_scenarios.pluck(:saved_scenario_id)
-      expect(updated_ids).to match_array([201, 202])
+      expect(updated_ids).to match_array([17, 18])
     end
   end
 
@@ -72,31 +74,9 @@ describe UpdateCollection, type: :service do
       expect(result).not_to be_successful
     end
 
-    it 'returns validation errors' do
-      expect(result.errors).to include(/scenario_ids/)
-    end
-
     it 'does not change the collection' do
       expect(collection.reload.title).not_to eq('')
-      expect(collection.scenarios.pluck(:scenario_id)).to eq([999])
-    end
-  end
-
-  context 'when saving the record fails with ActiveRecord::RecordInvalid' do
-    before do # TODO check/expand
-      allow_any_instance_of(Collection).to receive(:save!).and_raise(ActiveRecord::RecordInvalid.new(collection))
-    end
-
-    it 'returns a ServiceResult' do
-      expect(result).to be_a(ServiceResult)
-    end
-
-    it 'is not successful' do
-      expect(result).not_to be_successful
-    end
-
-    it 'returns the collection error messages' do
-      expect(result.errors).not_to be_empty # TODO check/expand
+      expect(collection.scenarios.pluck(:scenario_id)).to include(999)
     end
   end
 end
