37be53a17b62bb2f843505d104b2583f29aa0b19
noracato <ncschinkel@gmail.com>
2025-02-18 09:27:58 +0100

    Bring back `has_one` staff application to oauth apps

--- app/models/oauth_application.rb
+++ app/models/oauth_application.rb
@@ -4,5 +4,6 @@ class OAuthApplication < ApplicationRecord
   include Doorkeeper::Orm::ActiveRecord::Mixins::Application
 
   belongs_to :version
+  has_one :staff_application, foreign_key: :application_id, dependent: :destroy
   validates :uri, presence: true, 'doorkeeper/redirect_uri': true
 end
