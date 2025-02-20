dbdf25e048e532ceaf55eaee1fe2a538cbd52eaf
noracato <ncschinkel@gmail.com>
2025-02-20 16:37:29 +0100

    Let ETModel know to check for possible sign in on load

--- app/components/saved_scenarios/info/component.rb
+++ app/components/saved_scenarios/info/component.rb
@@ -6,11 +6,14 @@ module SavedScenarios::Info
     option :saved_scenario
     option :time
     option :button_title
+    option :with_user
 
     def link_to_scenario
       params = {
         scenario_id: @saved_scenario.scenario_id,
-        title: @saved_scenario.title
+        title: @saved_scenario.title,
+        # Let's etmodel know to check for signing in
+        current_user: @with_user
       }
 
       "#{@path}?#{params.to_query}"
