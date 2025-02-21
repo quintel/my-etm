52567aa0eb10d4beeffee81c529deebff6c81414
louispt1 <louisparkestalbot1@gmail.com>
2025-02-21 17:06:20 +0100

    Fixes for collaboration email.

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,13 @@
 module EmailHelper
   def email_inline_image_tag(image, **options)
-    attachments.inline[image] = File.read(Rails.root.join("app/assets/images/#{image}"))
-    image_tag(attachments[image].url, **options)
+    attachment = attachments.inline[image]
+
+    unless attachment
+      image_path = Rails.root.join("app/assets/images/#{image}")
+      attachment = attachments.inline[image] = File.read(image_path)
+    end
+
+    image_url = attachment.include?('@') ? "cid:#{attachment.split('@').first}" : "cid:#{attachment}"
+    image_tag(image_url, **options)
   end
 end
