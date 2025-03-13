f05cf57af6328451281f26b62646bb52ceecb288
noracato <ncschinkel@gmail.com>
2025-03-13 17:29:15 +0100

    Reconfigure Devise Mailer to use inline images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,10 +99,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  Rails.application.configure do
-    config.action_mailer.default_url_options = { host: 'my.energytransitionmodel.com', protocol: 'https' }
-    config.action_mailer.asset_host = 'https://my.energytransitionmodel.com'
-  end
-
 end
