55b912c983efa0ff43f3d472114bb20df5747901
louispt1 <louisparkestalbot1@gmail.com>
2025-04-28 11:06:35 +0200

    Updated mailers and added letter opener for development

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,12 +1,24 @@
 module EmailHelper
-  def email_inline_image_tag(image, **options)
-    attachment = attachments.inline[image]
-
-    unless attachment
-      image_path = Rails.root.join("app/assets/images/#{image}")
-      attachments.inline[image] = File.read(image_path)
+  def email_inline_image_tag(name, **options)
+    unless attachments.inline[name]
+      attachments.inline[name] = {
+        content: File.read(Rails.root.join("app/assets/images", name)),
+        mime_type: mime_type_for(name)
+      }
     end
 
-    image_tag(attachments[image].url, **options)
+    image_tag "cid:#{attachments[name].cid}", **options
+  end
+
+  private
+
+  def mime_type_for(filename)
+    case File.extname(filename).downcase
+    when '.png' then 'image/png'
+    when '.jpg', '.jpeg' then 'image/jpeg'
+    when '.gif' then 'image/gif'
+    when '.svg' then 'image/svg+xml'
+    else 'application/octet-stream'
+    end
   end
 end
