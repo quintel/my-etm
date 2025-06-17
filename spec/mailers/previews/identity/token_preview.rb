97b01ccdf49dc2c2dde050740576926b03bb45b8
Nora Schinkel <ncschinkel@gmail.com>
2025-06-17 11:36:37 +0200

    Update the token emails to match the other emails better

--- spec/mailers/previews/identity/token_preview.rb
+++ spec/mailers/previews/identity/token_preview.rb
@@ -2,6 +2,16 @@
 class Identity::TokenPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/identity/token/created_token
   def created_token
-    Identity::TokenMailer.created_token
+    Identity::TokenMailer.created_token(personal_access_token)
+  end
+
+  def expiring_token
+    Identity::TokenMailer.expiring_token(personal_access_token)
+  end
+
+  private
+
+  def personal_access_token
+    FactoryBot.create(:personal_access_token, user: User.all.first)
   end
 end
