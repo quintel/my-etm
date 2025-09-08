8212e91b3821ba4508c7653fcde533117b98752d
Nora Schinkel <ncschinkel@gmail.com>
2025-09-08 09:46:30 +0200

    Add a banner with a warning for dataset updates (#157)
    * Add a banner with a warning for dataset updates
    
    * Minor changes to dataset banner
    
    ---------
    
    Co-authored-by: Mathijs Bijkerk <mathijs.bijkerk@quintel.com>

--- app/components/notice_banner/component.rb
+++ app/components/notice_banner/component.rb
@@ -4,5 +4,30 @@ module NoticeBanner
     option :path, default: proc { "" }
     option :button_text, default: proc { "" }
     option :icon, default: proc { "information-circle" }
+    option :warning, default: proc { false }
+
+    def text_color
+      if warning
+        "text-midnight-200"
+      else
+        "text-midnight-800"
+      end
+    end
+
+    def icon_color
+      if warning
+        "text-midnight-200"
+      else
+        "text-midnight-450"
+      end
+    end
+
+    def bg_color
+      if warning
+        "bg-red-600"
+      else
+        "bg-midnight-300"
+      end
+    end
   end
 end
