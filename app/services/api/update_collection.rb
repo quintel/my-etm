5e5c3d6dc928d1d2ca27d45387187f515c3bcde1
aaccensi <andreu.accensi@gmail.com>
2025-11-04 14:14:48 +0100

    Use existing collection.update method. Move specs to the right spec file. Fix a typo.
    References #175

--- app/services/api/update_collection.rb
+++ app/services/api/update_collection.rb
@@ -52,11 +52,7 @@ module Api
     end
 
     def update_saved_scenarios(collection, saved_scenario_ids)
-      existing_ids = collection.saved_scenarios.pluck(:saved_scenario_id)
-      delete_ids   = existing_ids - saved_scenario_ids
-
-      collection.collection_saved_scenarios.delete_by(saved_scenario_id: delete_ids)
-      saved_scenario_ids.each.with_index(1) { |id, i| collection.collection_saved_scenarios.find_or_create_by(saved_scenario_id: id).update!(saved_scenario_order: i) }
+      collection.update(saved_scenario_ids:)
     end
   end
 end
