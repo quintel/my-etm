92d3c31b986c760a2e03b82598cf94820712935b
louispt1 <louisparkestalbot1@gmail.com>
2025-12-04 09:39:25 +0100

    Remove editable title stimulus controller, superceded by editable collections

--- app/components/collections/title/component.rb
+++ app/components/collections/title/component.rb
@@ -1,26 +1,5 @@
 module Collections::Title
   class Component < ApplicationComponent
-    include ButtonHelper
-
-    option :path
     option :title
-    option :editable
-
-    def css_classes
-      if @editable
-        'hover:underline
-        focus:outline-none
-        focus:border
-        focus:rounded-md
-        focus:px-2'
-      end
-    end
-
-    def actions
-      if @editable
-        'blur->editable-title#update
-        keydown.enter->editable-title#update'
-      end
-    end
   end
 end
