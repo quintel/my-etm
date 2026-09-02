b7db724de5922f673f8af5139eb34f1943e92a19
louispt1 <louisparkestalbot1@gmail.com>
2026-09-02 15:13:16 +0200

    Fix outputting logs under docker compose in development, following the pattern of staging/production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3002 }
 
+  # ETLauncher sets this. Without it, development logs go to log/development.log
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = ::Logger::Formatter.new
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
