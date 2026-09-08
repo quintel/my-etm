1b89b848560d15a260fc02fe94f4d19908eab844
Nora Schinkel <ncschinkel@gmail.com>
2026-09-08 18:44:03 +0200

    Keep url backwards compatible for collections version 2025-01

--- app/services/collection_url_builder.rb
+++ app/services/collection_url_builder.rb
@@ -12,6 +12,22 @@ module CollectionUrlBuilder
   #
   # Returns a string.
   def collections_app_url(collection)
-    "#{collection.version.collections_url}/collections/#{collection.id}?locale=#{I18n.locale}"
+    if collection.version.tag == "2025.01"
+      collections_app_url_backwards_compatible(collection)
+    else
+      "#{collection.version.collections_url}/collections/#{collection.id}?locale=#{I18n.locale}"
+    end
+  end
+
+  private
+
+  # Internal: One old version (2025-S1) uses straight session ids and a title param in the url
+  # instead of a collection ID
+  def collections_app_url_backwards_compatible(collection)
+    base = collection.version.collections_url
+    slug = collection.redirect_slug
+    query = "locale=#{I18n.locale}&title=#{ERB::Util.url_encode(collection.title)}"
+
+    "#{base}/#{slug}?#{query}"
   end
 end
